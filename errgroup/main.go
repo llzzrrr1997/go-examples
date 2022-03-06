2834162d2bf213b1ef6ac05ec83021af6b440168
llzzrrr1997 <llzzrrr1997@gmail.com>
2022-03-06 23:31:27 +0800

    errgrooup完善

--- errgroup/main.go
+++ errgroup/main.go
@@ -2,184 +2,313 @@ package main
 
 import (
 	"context"
-	"crypto/md5"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"golang.org/x/sync/errgroup"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"os"
-	"path/filepath"
+	"sync/atomic"
+	"time"
 )
 
-func main() {
-	demo2()
-}
+const (
+	nWorkers = 10
+)
 
-func demo1() {
-	g := new(errgroup.Group)
-	var urls = []string{
-		"http://www.golang.org/",
-		"http://www.google.com/",
-		"http://www.somestupidname.com/",
-	}
-	for _, url := range urls {
-		// Launch a goroutine to fetch the URL.
-		url := url // https://golang.org/doc/faq#closures_and_goroutines
-		g.Go(func() error {
-			// Fetch the URL.
-			resp, err := http.Get(url)
-			if err == nil {
-				resp.Body.Close()
-			}
-			return err
-		})
-	}
-	// Wait for all HTTP fetches to complete.
-	err := g.Wait()
-	if err == nil {
-		fmt.Println("Successfully fetched all URLs.")
-	} else {
-		fmt.Println(err)
-	}
-}
-
-//demo2
 var (
-	Web   = fakeSearch("web")
-	Image = fakeSearch("image")
-	Video = fakeSearch("video")
+	//好友数组
+	users        = []string{"aa", "bb", "cc", "dd", "ee", "ff"}
+	errorEnd     = errors.New("query user end")
+	errorUnknown = errors.New("unknown err")
 )
 
-type Result string
-type Search func(ctx context.Context, query string) (Result, error)
+type User struct {
+	Id   int64
+	Name string
+}
 
-func fakeSearch(kind string) Search {
-	return func(_ context.Context, query string) (Result, error) {
-		return Result(fmt.Sprintf("%s result for %q", kind, query)), nil
+//朋友关系迭代器
+type FriendIterator struct {
+	index int64
+}
+
+//获取下一个朋友id
+func (it *FriendIterator) Next(ctx context.Context) (int64, error) {
+	if it.index >= int64(len(users)) {
+		return 0, errorEnd
+	}
+
+	query := time.After(10 * time.Millisecond) //模拟查询耗时
+	//模拟查询出错
+	/*if it.index == 3 {
+		return it.index, errorUnknown
+	}*/
+	//模拟慢查询
+	/*if it.index == 4 {
+		query = time.After(10*time.Second)
+		fmt.Printf("slow query.\n")
+	}*/
+
+	// 10ms返回数据
+	select {
+	case <-ctx.Done():
+		return it.index, ctx.Err()
+	case <-query:
+		r := it.index
+		it.index++
+		fmt.Printf("found friend id: %d\n", r)
+		return r, nil
 	}
 }
 
-func errSearch() Search {
-	return func(ctx context.Context, query string) (Result, error) {
-		return "", errors.New("search err")
-	}
+func GetFriendIds(user int64) *FriendIterator {
+	return &FriendIterator{}
 }
-func demo2() {
-	Google := func(ctx context.Context, query string) ([]Result, error) {
-		g, ctx := errgroup.WithContext(ctx)
 
-		searches := []Search{Web, Image, Video}
-		results := make([]Result, len(searches))
-		for i, search := range searches {
-			i, search := i, search // https://golang.org/doc/faq#closures_and_goroutines
-			g.Go(func() error {
-				result, err := search(ctx, query)
-				if err == nil {
-					results[i] = result
-				}
-				return err
-			})
+//获取用户的信息
+func GetUserProfile(ctx context.Context, id int64) (*User, error) {
+	// 100ms返回数据
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case <-time.After(100 * time.Millisecond):
+		if id < 0 || id >= int64(len(users)) {
+			return nil, fmt.Errorf("unknown user: %d", id)
 		}
-		if err := g.Wait(); err != nil {
-			return nil, err
-		}
-		return results, nil
-	}
-
-	results, err := Google(context.Background(), "golang")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
-	for _, result := range results {
-		fmt.Println(result)
+		fmt.Printf("found user profile: %d\n", id)
+		return &User{Id: id, Name: users[id]}, nil
 	}
 }
 
-//demo3
+//假设现在有两个服务，一个是朋友关系服务，一个是用户信息服务。
+//现在有个接口要获取某个用户的朋友信息
+func main() {
 
-// Pipeline demonstrates the use of a Group to implement a multi-stage
-// pipeline: a version of the MD5All function with bounded parallelism from
-// https://blog.golang.org/pipelines.
-func demo3() {
-	m, err := MD5All(context.Background(), ".")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for k, sum := range m {
-		fmt.Printf("%s:\t%x\n", k, sum)
-	}
-}
-
-type result struct {
-	path string
-	sum  [md5.Size]byte
-}
-
-// MD5All reads all the files in the file tree rooted at root and returns a map
-// from file path to the MD5 sum of the file's contents. If the directory walk
-// fails or any read operation fails, MD5All returns an error.
-func MD5All(ctx context.Context, root string) (map[string][md5.Size]byte, error) {
-	// ctx is canceled when g.Wait() returns. When this version of MD5All returns
-	// - even in case of error! - we know that all of the goroutines have finished
-	// and the memory they were using can be garbage-collected.
-	g, ctx := errgroup.WithContext(ctx)
-	paths := make(chan string)
-
-	g.Go(func() error {
-		defer close(paths)
-		return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if !info.Mode().IsRegular() {
-				return nil
-			}
-			select {
-			case paths <- path:
-			case <-ctx.Done():
-				return ctx.Err()
-			}
-			return nil
-		})
-	})
-
-	// Start a fixed number of goroutines to read and digest files.
-	c := make(chan result)
-	const numDigesters = 20
-	for i := 0; i < numDigesters; i++ {
-		g.Go(func() error {
-			for path := range paths {
-				data, err := ioutil.ReadFile(path)
-				if err != nil {
-					return err
-				}
-				select {
-				case c <- result{path, md5.Sum(data)}:
-				case <-ctx.Done():
-					return ctx.Err()
-				}
-			}
-			return nil
-		})
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 1500*time.Millisecond)
 	go func() {
-		g.Wait()
-		close(c)
+		time.Sleep(2000 * time.Millisecond) //模拟总请求时间为2s
+		fmt.Printf("cancel \n")
+		cancel()
+	}()
+	start := time.Now()
+	rsp, err := demo1(ctx, 0)
+	//rsp, err := demo2(ctx,0)
+	//rsp, err := demo3(ctx,0)
+	//rsp, err := demo4(ctx,0)
+	if err != nil {
+		fmt.Printf("error: %s", err)
+	} else {
+		fmt.Printf("finished in %s\n ret: %s", time.Since(start), jsonString(rsp))
+	}
+	fmt.Println()
+
+}
+
+func jsonString(v interface{}) string {
+	if ret, err := json.Marshal(v); err != nil {
+		return err.Error()
+	} else {
+		return string(ret)
+	}
+}
+
+//串行操作  先查询朋友ID再查询朋友信息
+func demo1(ctx context.Context, user int64) (map[string]*User, error) {
+	// 获取朋友id
+	var friendIds []int64
+	for it := GetFriendIds(user); ; {
+		if id, err := it.Next(ctx); err != nil {
+			if err == errorEnd {
+				break
+			}
+			return nil, fmt.Errorf("GetFriendIds %d: %s", id, err)
+		} else {
+			friendIds = append(friendIds, id)
+		}
+	}
+
+	// 查询朋友的信息
+	ret := map[string]*User{}
+	for _, friendId := range friendIds {
+		if friend, err := GetUserProfile(ctx, friendId); err != nil {
+			return nil, fmt.Errorf("GetUserProfile %d: %s", friendId, err)
+		} else {
+			ret[friend.Name] = friend
+		}
+	}
+	return ret, nil
+}
+
+//并行处理
+func demo2(ctx context.Context, user int64) (map[string]*User, error) {
+	friendIds := make(chan int64)
+
+	// 有一个Goroutine去获取朋友id
+	go func() {
+		defer close(friendIds)
+		for it := GetFriendIds(user); ; {
+			if id, err := it.Next(ctx); err != nil {
+				if err == errorEnd {
+					break
+				}
+				//如果出错了，该怎么处理err呢
+				log.Fatalf("GetFriendIds %d: %s", id, err)
+			} else {
+				friendIds <- id
+			}
+		}
 	}()
 
-	m := make(map[string][md5.Size]byte)
-	for r := range c {
-		m[r.path] = r.sum
+	friends := make(chan *User)
+
+	// 有n个Goroutine去获取朋友信息
+	workers := int32(nWorkers)
+	for i := 0; i < nWorkers; i++ {
+		go func() {
+			defer func() {
+				// 关闭channel
+				if atomic.AddInt32(&workers, -1) == 0 {
+					close(friends)
+				}
+			}()
+
+			for id := range friendIds {
+				if friend, err := GetUserProfile(ctx, id); err != nil {
+					//如果出错了，该怎么处理err呢
+					log.Fatalf("GetUserProfile %d: %s", id, err)
+				} else {
+					friends <- friend
+				}
+			}
+		}()
 	}
-	// Check whether any of the goroutines failed. Since g is accumulating the
-	// errors, we don't need to send them (or check for them) in the individual
-	// results sent on the channel.
-	if err := g.Wait(); err != nil {
-		return nil, err
+
+	//封装返回信息
+	ret := map[string]*User{}
+	for friend := range friends {
+		ret[friend.Name] = friend
 	}
-	return m, nil
+
+	return ret, nil
+}
+
+//使用errgroup来并行处理
+func demo3(ctx context.Context, user int64) (map[string]*User, error) {
+	g := errgroup.Group{}
+	friendIds := make(chan int64)
+
+	// 有一个Goroutine去获取朋友id
+	g.Go(func() error {
+		defer close(friendIds)
+		for it := GetFriendIds(user); ; {
+			if id, err := it.Next(ctx); err != nil {
+				if err == errorEnd {
+					return nil
+				}
+				return fmt.Errorf("GetFriendIds %d: %s", id, err)
+			} else {
+				friendIds <- id
+			}
+		}
+	})
+
+	friends := make(chan *User)
+
+	//有n个Goroutine去获取朋友信息
+	workers := int32(nWorkers)
+	for i := 0; i < nWorkers; i++ {
+		g.Go(func() error {
+			defer func() {
+				// 关闭channel
+				if atomic.AddInt32(&workers, -1) == 0 {
+					close(friends)
+				}
+			}()
+
+			for id := range friendIds {
+				if friend, err := GetUserProfile(ctx, id); err != nil {
+					return fmt.Errorf("GetUserProfile %d: %s", id, err)
+				} else {
+					friends <- friend
+				}
+			}
+			return nil
+		})
+	}
+
+	// 还有一个Goroutine去封装返回信息
+	ret := map[string]*User{}
+	g.Go(func() error {
+		for friend := range friends {
+			ret[friend.Name] = friend
+		}
+		return nil
+	})
+
+	// 返回数据 g.Wait()会返回Goroutine执行出错的第一个err
+	return ret, g.Wait()
+}
+
+func demo4(ctx context.Context, user int64) (map[string]*User, error) {
+	g, ctx := errgroup.WithContext(ctx)
+	friendIds := make(chan int64)
+
+	//有一个Goroutine去获取朋友id
+	g.Go(func() error {
+		defer close(friendIds)
+		for it := GetFriendIds(user); ; {
+			if id, err := it.Next(ctx); err != nil {
+				if err == errorEnd {
+					return nil
+				}
+				return fmt.Errorf("GetFriendIds %d: %s", id, err)
+			} else {
+				select { //使用select来退出Goroutine
+				case <-ctx.Done():
+					return ctx.Err()
+				case friendIds <- id:
+				}
+			}
+		}
+	})
+
+	friends := make(chan *User)
+
+	//有n个Goroutine去获取朋友信息
+	workers := int32(nWorkers)
+	for i := 0; i < nWorkers; i++ {
+		g.Go(func() error {
+			defer func() {
+				// 关闭channel
+				if atomic.AddInt32(&workers, -1) == 0 {
+					close(friends)
+				}
+			}()
+
+			for id := range friendIds {
+				if friend, err := GetUserProfile(ctx, id); err != nil {
+					return fmt.Errorf("GetUserProfile %d: %s", id, err)
+				} else {
+					select { //使用select来退出Goroutine
+					case <-ctx.Done():
+						return ctx.Err()
+					case friends <- friend:
+					}
+				}
+			}
+			return nil
+		})
+	}
+
+	//还有一个Goroutine去封装返回信息
+	ret := map[string]*User{}
+	g.Go(func() error {
+		for friend := range friends {
+			ret[friend.Name] = friend
+		}
+		return nil
+	})
+
+	//返回数据 g.Wait()会返回Goroutine执行出错的第一个err
+	return ret, g.Wait()
 }
